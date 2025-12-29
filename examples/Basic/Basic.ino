8d363b276e4462322db096829d77c42179757098
marinpopa <popica1965@yahoo.com>
2025-12-29 12:29:17 +0200

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,15 +1,29 @@
+#include <WiFi.h>
 #include <AsyncOTAManager.h>
 
 AsyncWebServer server(80);
 AsyncOTAManager otaManager(server);
 
 void setup() {
-  otaManager.setAuthentication("admin", "password123");
-  otaManager.enableAutoDetect(true);
-  otaManager.begin("/ota");
+  Serial.begin(115200);
+  
+  // Connect to WiFi
+  WiFi.begin("SSID", "password");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("\nConnected! IP: " + WiFi.localIP().toString());
+  
+  // Configure OTA
+  otaManager.setAuthentication("admin", "password123"); // Optional
+  otaManager.enableAutoDetect(true); // Enabled by default
+  otaManager.begin("/update");
+  
   server.begin();
 }
 
 void loop() {
-  // Codul tău principal
+  // Your main code
 }
